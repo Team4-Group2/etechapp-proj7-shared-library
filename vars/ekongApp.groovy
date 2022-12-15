1f54617513e8a5d6f41efc1d740350f7ad818a25
InemesitEkong <inemekong@gmail.com>
2022-12-15 17:59:27 -0500

    added ekongApp groovy file
    Signed-off-by: InemesitEkong <inemekong@gmail.com>

---
+++ vars/ekongApp.groovy
@@ -0,0 +1,28 @@
+def call(String repoUrl){
+pipeline {
+       agent any
+       stages {
+           stage{
+                Parallel{
+                    stage("Tools initialization") {
+                        steps {
+                            sh 'lscpu'
+                            sh 'java -version'
+                        }
+                    }
+                    stage("Checkout Code") {
+                        steps {
+                            git branch: 'main',
+                                    url: "${repoUrl}"
+                        }
+                    }
+                }
+            }
+            stage("to-test-maven") {
+               steps {
+                   sh 'df -h'
+               }
+           }
+       }
+}
+}
